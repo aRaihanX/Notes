1dc2efd7ee16baf329df6aa3a2d2c3a817fcb61a
Raihan <71235716+aRaihanX@users.noreply.github.com>
2022-06-21 00:03:31 +0600

    Update Arch_install.sh

--- Arch_install.sh
+++ Arch_install.sh
@@ -4,6 +4,8 @@ iwctl #Wifi setting
     station wlan0123 get-networks #Lists all networks
     station wlan0123 connect "Network Name" #Connect to wifi
 mount /dev/sda1 /mnt
+vim /etc/pacman.d/mirrorlist
+    Server = http://mirror.xeonbd.com/archlinux/$repo/os/$arch
 
 pacstrap /mnt base base-devel linux linux-firmware linux-headers vim grub networkmanager network-manager-applet
 genfstab -U /mnt >> /mnt/etc/fstab
