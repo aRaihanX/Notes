0146b4d30d8e778e77006cca897ad7971b9734d5
Raihan <71235716+aRaihanX@users.noreply.github.com>
2022-07-13 02:00:00 +0600

    Update Arch_install.sh

--- Arch_install.sh
+++ Arch_install.sh
@@ -10,7 +10,7 @@ vim /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel linux linux-firmware linux-headers vim grub networkmanager network-manager-applet
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
-vim /etc/locae.gen
+vim /etc/locale.gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 locale-gen
 echo "Hostname" > /etc/hostname
